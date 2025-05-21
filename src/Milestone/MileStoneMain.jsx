a81255fd05b4d828363653e224347774295ce7f4
helloakshay27 <helloakshay27@gmail.com>
2025-05-21 18:24:31 +0530

    feat: Enhance task management with dependency linking and visual representation
    - Added functionality to handle multiple task dependencies with arrows using react-xarrows.
    - Updated BoardsSection to manage arrow links and toggle visibility based on task dependencies.
    - Modified TaskCard to include a clickable SVG for linking tasks.
    - Enhanced data structure to support task dependencies in the Data.js file.
    - Improved styling in index.css to ensure proper layering of SVG elements.

--- src/Milestone/MileStoneMain.jsx
+++ src/Milestone/MileStoneMain.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import MilestoneHeader from './MilestoneHeader';
 import MilestoneBody from './MilestoneBody';
 import DateMilestone from './weekProgressPicker';
+import NodeConnection from './nodeconnection';
 
 const MileStoneMain = () => {
     return (
         <div>
             <MilestoneHeader />
             <MilestoneBody />
-            <DateMilestone/>
+            {/* <DateMilestone/> */}
+            {/* <NodeConnection /> */}
         </div>
     );
 };
