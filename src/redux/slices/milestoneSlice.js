129c5bb77f8c4002028bd0f7aa272c87d4ed8c53
atharv0811 <karnekaratharv12@gmail.com>
2025-05-30 17:42:26 +0530

    milestone slice added

---
+++ src/redux/slices/milestoneSlice.js
@@ -0,0 +1,53 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const access_token = 'bTcVnWgQrF6QCdNbMiPXzCZNAqsN9qoEfFWdFQ1Auk4'
+
+const createApiSlice = (name, fetchThunk) => createSlice({
+    name,
+    initialState: {
+        loading: false,
+        success: false,
+        error: null,
+        [name]: [],
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchThunk.pending, (state) => {
+                state.loading = true;
+                state.success = false;
+                state.error = null;
+            })
+            .addCase(fetchThunk.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+                state.error = null;
+                state[name] = action.payload;
+            })
+            .addCase(fetchThunk.rejected, (state, action) => {
+                state.loading = false;
+                state.success = false;
+                state.error = action.payload || action.error.message;
+            });
+    },
+});
+
+export const createMilestone = createAsyncThunk('createMilestone', async ({ payload }) => {
+    try {
+        const response = await axios.post(`https://api-tasks.lockated.com/milestones.json`, payload, {
+            headers: {
+                Authorization: `Bearer ${access_token}`,
+            }
+        })
+
+        return response.data
+    } catch (error) {
+        console.log(error)
+        return error.response.data
+    }
+})
+
+export const createMilestoneSlice = createApiSlice('createMilestone', createMilestone);
+
+export const createMilestoneReducer = createMilestoneSlice.reducer;
