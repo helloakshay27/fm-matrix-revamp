c5d56f9acf4ee67e9ac95ba5a809d1562079c221
helloakshay27 <helloakshay27@gmail.com>
2025-06-10 09:11:40 +0530

    feat: Implement modal for adding/editing project statuses with API integration

--- src/pages/Setup/Status.jsx
+++ src/pages/Setup/Status.jsx
@@ -3,14 +3,14 @@ import StatusTable from '../../components/Setup/Status/Table.jsx';
 import Modal from '../../components/Setup/Status/Modal.jsx';
 
 const Status = () => {
-      const [openModal, setOpenModal] = useState(false);
-  return(
-   <div className="flex flex-col gap-2 p-10 text-[14px]">
+    const [openModal, setOpenModal] = useState(false);
+    return (
+        <div className="flex flex-col gap-2 p-10 text-[14px]">
             <div className="flex justify-end ">
-               <button className="h-[38px] w-[170px] bg-[#C72030] text-white mr-5" onClick={()=>{setOpenModal(true)}}>+ Add Status</button>
+                <button className="h-[38px] w-[170px] bg-[#C72030] text-white mr-5" onClick={() => { setOpenModal(true) }}>+ Add Status</button>
             </div>
             <StatusTable />
-            {openModal && <Modal setOpenModal={setOpenModal} openModal={openModal}/>}
+            {openModal && <Modal setOpenModal={setOpenModal} openModal={openModal} />}
         </div>
     )
 }
