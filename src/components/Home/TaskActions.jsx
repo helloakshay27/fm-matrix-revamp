52dbb9b22a7bced1480e3b6009f74ba031cbe62a
helloakshay27 <helloakshay27@gmail.com>
2025-06-12 01:43:02 +0530

    Fix conditional rendering in TaskActions: remove duplicate check for Sprint-Gantt in type dropdown

--- src/components/Home/TaskActions.jsx
+++ src/components/Home/TaskActions.jsx
@@ -226,7 +226,7 @@ const TaskActions = ({
         <>
             <div className="flex items-center justify-end mx-4 my-3 text-sm">
                 <div className="flex items-center gap-3 divide-x divide-gray-400">
-                    {addType !== "Issues" && addType !== "Sprint-Gantt"&& addType !== "Sprint-Gantt"&& !["Milestone", "Project", "Task","templates","archived"].includes(addType)  && renderTypeDropdown()}
+                    {addType !== "Issues" && addType !== "Sprint-Gantt"&& addType !== "Sprint-Gantt"&& !["Milestone", "Project","templates","archived"].includes(addType)  && renderTypeDropdown()}
                     {addType !== "Issues" && !["Milestone", "Project", "Task", "active_projects","templates","archived"].includes(addType) && renderSprintTypeDropdown()}
                     {addType !== "Milestone" && (
                         <div
