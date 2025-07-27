5e29a4d019ffc0646c9f3f06c702a2923a16fa47
atharv0811 <karnekaratharv12@gmail.com>
2025-07-28 03:49:05 +0530

    add currency in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -494,10 +494,31 @@ import { PasswordResetSuccessPage } from '@/pages/PasswordResetSuccessPage';
 import { isAuthenticated } from '@/utils/auth';
 import { BookingDetailsPage } from './pages/BookingDetailsPage';
 import { RestaurantOrdersTable } from './components/RestaurantOrdersTable';
+import { useAppDispatch, useAppSelector } from './store/hooks';
+import { useEffect } from 'react';
+import { getCurrency } from './store/slices/currencySlice';
+// import CurrencyDashboard from './pages/setup/CurrencyDashboard';
 
 const queryClient = new QueryClient();
 
 function App() {
+  const dispatch = useAppDispatch();
+  const baseUrl = localStorage.getItem('baseUrl');
+  const token = localStorage.getItem('token');
+
+  useEffect(() => {
+    const fetchCurrency = async () => {
+      try {
+        const response = await dispatch(getCurrency({ baseUrl, token })).unwrap();
+        localStorage.setItem('currency', response[0].value)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchCurrency();
+  }, [])
+
   return (
     <Provider store={store}>
       <Router>
@@ -938,6 +959,7 @@ function App() {
                 <Route path="/settings/vas/booking/setup/add" element={<AddBookingSetupPage />} />
                 <Route path="/settings/vas/booking/setup/details/:id" element={<BookingSetupDetailPage />} />
                 <Route path="/settings/waste-management/setup" element={<UtilityWasteGenerationSetupDashboard />} />
+                {/* <Route path="/settings/currency" element={<CurrencyDashboard />} /> */}
               </Route>
 
               {/* Setup Routes - Outside of settings parent route */}
@@ -950,20 +972,20 @@ function App() {
               <Route path="/setup/occupant-users" element={<ProtectedRoute><OccupantUsersDashboard /></ProtectedRoute>} />
               <Route path="/setup/occupant-users/add" element={<ProtectedRoute><AddOccupantUserDashboard /></ProtectedRoute>} />
 
-                
-                {/* Mobile Routes */}
-                <Route path="/mobile/tickets" element={<MobileTicketsPage />} />
-                <Route path="/mobile/orders" element={<MobileOrdersPage />} />
-                {/* Mobile Restaurant Routes */}
-                <Route path="/mobile/restaurant" element={<MobileRestaurantPage />} />
-                <Route path="/mobile/restaurant/:action" element={<MobileRestaurantPage />} />
-                <Route path="/mobile/restaurant/:restaurantId/:action" element={<MobileRestaurantPage />} />
-                {/* Mobile Asset Routes */}
-                <Route path="/mobile/assets" element={<MobileAssetPage />} />
-                <Route path="/mobile/assets/:assetId" element={<MobileAssetPage />} />
-                {/* QR Test Route */}
-                <Route path="/qr-test" element={<QRTestPage />} />
-              </Routes>
+
+              {/* Mobile Routes */}
+              <Route path="/mobile/tickets" element={<MobileTicketsPage />} />
+              <Route path="/mobile/orders" element={<MobileOrdersPage />} />
+              {/* Mobile Restaurant Routes */}
+              <Route path="/mobile/restaurant" element={<MobileRestaurantPage />} />
+              <Route path="/mobile/restaurant/:action" element={<MobileRestaurantPage />} />
+              <Route path="/mobile/restaurant/:restaurantId/:action" element={<MobileRestaurantPage />} />
+              {/* Mobile Asset Routes */}
+              <Route path="/mobile/assets" element={<MobileAssetPage />} />
+              <Route path="/mobile/assets/:assetId" element={<MobileAssetPage />} />
+              {/* QR Test Route */}
+              <Route path="/qr-test" element={<QRTestPage />} />
+            </Routes>
             <Toaster />
             <SonnerToaster
               position="top-right"
