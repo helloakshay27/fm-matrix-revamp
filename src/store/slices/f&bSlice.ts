bc6d052e0b943e1662232b1b323abce1232f3f4e
atharv0811 <karnekaratharv12@gmail.com>
2025-08-01 18:20:36 +0530

    changes

--- src/store/slices/f&bSlice.ts
+++ src/store/slices/f&bSlice.ts
@@ -601,10 +601,10 @@ export const exportOrders = createAsyncThunk(
     async ({ baseUrl, token, id }: { baseUrl: string; token: string; id: number }, { rejectWithValue }) => {
         try {
             const response = await axios.get(
-                `https://${baseUrl}/pms/admin/restaurants/${id}/food_orders.xlsx`,
+                `https://${baseUrl}/pms/admin/restaurants/${id}/food_orders.xlsx?access_token=${token}`,
                 {
                     headers: {
-                        Authorization: `Bearer ${token}`,
+                        // Authorization: `Bearer ${token}`,
                         Accept: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
                     },
                 }
