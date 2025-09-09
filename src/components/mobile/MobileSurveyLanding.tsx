c2598cfc45790fc676b43c11e0151b4ba3955528
akshay <akshaymugale1@gnmail.com>
2025-09-09 15:08:28 +0530

    smiley emoji

--- src/components/mobile/MobileSurveyLanding.tsx
+++ src/components/mobile/MobileSurveyLanding.tsx
@@ -143,6 +143,7 @@ export const MobileSurveyLanding: React.FC = () => {
       case 'rating':
         return selectedRating !== null;
       case 'emoji':
+      case 'smiley':
         return selectedRating !== null;
       default:
         return true;
