3351be1b91a2950a01a0912e5e1673f8af0b3ddf
atharv0811 <karnekaratharv12@gmail.com>
2025-09-15 12:17:12 +0530

    style(Dashboards): enhance TextField components across multiple pages by improving layout and input handling for better user experience

--- src/pages/WorkOrderAddPage.tsx
+++ src/pages/WorkOrderAddPage.tsx
@@ -779,6 +779,18 @@ export const WorkOrderAddPage: React.FC = () => {
                     "& .MuiOutlinedInput-root": {
                       height: "auto !important",
                       padding: "2px !important",
+                      display: "flex",
+                    },
+                    "& .MuiInputBase-input[aria-hidden='true']": {
+                      flex: 0,
+                      width: 0,
+                      height: 0,
+                      padding: "0 !important",
+                      margin: 0,
+                      display: "none",
+                    },
+                    "& .MuiInputBase-input": {
+                      resize: "none !important",
                     },
                   }}
                 />
