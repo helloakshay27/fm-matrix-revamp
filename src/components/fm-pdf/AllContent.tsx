03d491939b25b1db87034aad6d30661c8798fe82
Rahul Parihar <rahulpariharmalii@gmail.com>
2025-09-29 11:30:01 +0530

    Refactor striped pattern IDs for clarity and adjust chart margins for better layout in AllContent component

--- src/components/fm-pdf/AllContent.tsx
+++ src/components/fm-pdf/AllContent.tsx
@@ -3004,10 +3004,10 @@ const AllContent = () => {
                             layout="vertical"
                             margin={{ top: 5, right: 30, left: 10, bottom: 5 }}
                         >
-                            {/* 1. Add pattern defs for striped fill */}
+                            {/* 1. Add pattern defs for striped fill (unique id for Response) */}
                             <defs>
                                 <pattern
-                                    id="stripedPattern"
+                                    id="stripedPatternResponse"
                                     patternUnits="userSpaceOnUse"
                                     width="6"
                                     height="6"
@@ -3043,7 +3043,7 @@ const AllContent = () => {
                             {/* 3. Apply striped pattern to Last Quarter bar */}
                             <Bar
                                 dataKey="responseLast"
-                                fill="url(#stripedPattern)"
+                                fill="url(#stripedPatternResponse)"
                                 name={lastLabel}
                             />
 
@@ -3149,16 +3149,16 @@ const AllContent = () => {
 
                     <div className="w-full overflow-x-auto block print:block md:hidden">
                         <BarChart
-                            width={780}
+                            width={750}
                             height={700}
                             data={dynamicResolutionAchieved}
                             layout="vertical"
                             margin={{ top: 5, right: 30, left: 10, bottom: 5 }}
                         >
-                            {/* Striped Pattern Def */}
+                            {/* Striped Pattern Def (unique id for Resolution) */}
                             <defs>
                                 <pattern
-                                    id="stripedPattern"
+                                    id="stripedPatternResolution"
                                     patternUnits="userSpaceOnUse"
                                     width="6"
                                     height="6"
@@ -3188,12 +3188,12 @@ const AllContent = () => {
                                 width={140}
                             />
 
-                            <Tooltip content={(props: any) => <CustomTooltip {...props} />} cursor={{ fill: "rgba(0, 0, 0, 0.05)" }} />
+                            <Tooltip content={(props: any) => <CustomTooltip {...props} />} />
 
                             {/* Last Quarter with Stripes */}
                             <Bar
                                 dataKey="resolutionLast"
-                                fill="url(#stripedPattern)"
+                                fill="url(#stripedPatternResolution)"
                                 name={lastLabel}
                             />
 
@@ -3202,20 +3202,6 @@ const AllContent = () => {
                                 dataKey="resolutionCurrent"
                                 fill="#C4AE9D"
                                 name={currentLabel}
-                            />
-
-                            {/* Scatter dots (optional, will still render over bars) */}
-                            <Scatter
-                                data={scatterDataLast}
-                                fill="#000000"
-                                name={`${lastLabel} (Dots)`}
-                                shape="circle"
-                            />
-                            <Scatter
-                                data={scatterDataCurrent}
-                                fill="#FF0000"
-                                name={`${currentLabel} (Dots)`}
-                                shape="circle"
                             />
                         </BarChart>
                     </div>
@@ -4041,7 +4027,7 @@ const AllContent = () => {
                                             <BarChart
                                                 data={parkingChartData}
                                                 layout="vertical"
-                                                margin={{ top: 20, right: 30, left: 10, bottom: 20 }}
+                                                margin={{ top: 20, right: 70, bottom: 20 }}
                                                 barCategoryGap="10%"
                                                 barGap={3}
                                             >
@@ -4075,7 +4061,7 @@ const AllContent = () => {
                                         height={Math.max(300, parkingChartData.length * 50)}
                                         data={parkingChartData}
                                         layout="vertical"
-                                        margin={{ top: 20, right: 30, left: 10, bottom: 20 }}
+                                        margin={{ top: 20, right: 60, left: 10, bottom: 20 }}
                                         barCategoryGap="10%"
                                         barGap={3}
                                     >
