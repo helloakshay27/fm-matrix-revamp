105a9f1b526a77bc6b6c01324e5b5e0fa55a1dc5
TEJAS CHAUDHARI <tejasrc201920@gmail.com>
2025-10-08 15:34:06 +0530

    permit details change

--- src/pages/PermitDetails.tsx
+++ src/pages/PermitDetails.tsx
@@ -2631,7 +2631,24 @@ export const PermitDetails = () => {
                                             InputLabelProps={{
                                                 shrink: true
                                             }}
-                                            sx={fieldStyles}
+                                            sx={{
+                                                "& .MuiOutlinedInput-root": {
+                                                    height: "auto !important",
+                                                    padding: "2px !important",
+                                                    display: "flex",
+                                                },
+                                                "& .MuiInputBase-input[aria-hidden='true']": {
+                                                    flex: 0,
+                                                    width: 0,
+                                                    height: 0,
+                                                    padding: "0 !important",
+                                                    margin: 0,
+                                                    display: "none",
+                                                },
+                                                "& .MuiInputBase-input": {
+                                                    resize: "none !important",
+                                                },
+                                            }}
                                         />
                                     </div>
 
