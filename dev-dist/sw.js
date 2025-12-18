10c6f70cb3062075259000e1a062fd3f79247e91
atharv0811 <karnekaratharv12@gmail.com>
2025-12-18 16:48:56 +0530

    refactor(App): remove websocket connection and notification logic
    Temporarily disable websocket functionality for maintenance and debugging purposes. The service worker cache revision was also updated as part of the build process.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dpohv6bk9k8"
+    "revision": "0.l9vpi0iv0to"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
