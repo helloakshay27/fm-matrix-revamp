df4fb8b7924f9b254641228a2aa3de3f209f93d9
atharv0811 <karnekaratharv12@gmail.com>
2025-12-27 01:21:40 +0530

    refactor(AddMilestoneForm): add type annotation for props parameter

--- src/components/AddMilestoneForm.tsx
+++ src/components/AddMilestoneForm.tsx
@@ -268,7 +268,7 @@ const AddMilestoneModal = ({
     );
 };
 
-const AddMilestoneForm = ({ owners, projects, handleClose, className = "max-w-[90%] mx-auto", prefillData, isConversion = false, opportunityId, onSuccess }) => {
+const AddMilestoneForm = ({ owners, projects, handleClose, className = "max-w-[90%] mx-auto", prefillData, isConversion = false, opportunityId, onSuccess }: any) => {
     const dispatch = useAppDispatch();
     const token = localStorage.getItem("token");
     const baseUrl = localStorage.getItem("baseUrl");
