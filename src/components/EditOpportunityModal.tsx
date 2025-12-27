39d82d6cf2b01c3b35eb59e5a64df4d23c8eb68b
atharv0811 <karnekaratharv12@gmail.com>
2025-12-27 12:02:09 +0530

    feat(EditOpportunityModal): add tag creation functionality
    Add a modal for creating new tags in the opportunity editor. The modal can be triggered via a "Create new tag" link and will refresh the tag list after creation.

--- src/components/EditOpportunityModal.tsx
+++ src/components/EditOpportunityModal.tsx
@@ -20,6 +20,7 @@ import axios from 'axios';
 import { getFullUrl } from '../config/apiConfig';
 import { Mention, MentionsInput } from 'react-mentions';
 import MuiMultiSelect from './MuiMultiSelect';
+import { AddTagModal } from './AddTagModal';
 
 const Transition = forwardRef(function Transition(
     props: TransitionProps & { children: React.ReactElement },
@@ -57,6 +58,7 @@ const EditOpportunityModal: React.FC<EditOpportunityModalProps> = ({ open, onClo
     const [removedAttachmentIds, setRemovedAttachmentIds] = useState<number[]>([]);
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
+    const [isTagModalOpen, setIsTagModalOpen] = useState(false);
 
     // Mention state
     const [mentionUsers, setMentionUsers] = useState<any[]>([]);
@@ -426,6 +428,12 @@ const EditOpportunityModal: React.FC<EditOpportunityModalProps> = ({ open, onClo
                         </div>
 
                         <div>
+                            <div
+                                className="text-[12px] text-[red] text-right cursor-pointer mb-2"
+                                onClick={() => setIsTagModalOpen(true)}
+                            >
+                                <i>Create new tag</i>
+                            </div>
                             <MuiMultiSelect
                                 label="Tags"
                                 options={mentionTags.map((tag) => ({ value: tag.id, label: tag.name, id: tag.id }))}
@@ -529,6 +537,11 @@ const EditOpportunityModal: React.FC<EditOpportunityModalProps> = ({ open, onClo
                     </Button>
                 </div>
             </DialogContent>
+            <AddTagModal
+                isOpen={isTagModalOpen}
+                onClose={() => setIsTagModalOpen(false)}
+                onTagCreated={() => fetchMentionTags()}
+            />
         </Dialog>
     );
 };
