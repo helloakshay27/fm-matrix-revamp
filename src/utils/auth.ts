df828c69ba94feab2da90928bf1b19e3da986b1b
atharv0811 <karnekaratharv12@gmail.com>
2026-01-17 13:56:44 +0530

    fix: update API endpoint for fetching organizations by email

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -212,7 +212,7 @@ export const getOrganizationsByEmail = async (
 
   // Default fallback for other sitess
   const response = await fetch(
-    `https://pulse-api.lockated.com/api/users/get_organizations_by_email.json?email=${email}`
+    `https://fm-uat-api.lockated.com/api/users/get_organizations_by_email.json?email=${email}`
   );
 
   if (!response.ok) {
