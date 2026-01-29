da27366904cf6bfdc402f15981c17199e33e1b69
Omkar Chavan <omkarchavan3798@gmail.com>
2026-01-29 17:13:52 +0530

    removed specific id to access asset module

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -245,6 +245,7 @@ const ASSET_RESTRICTED_EMAILS = [
   "reception1@gmail.com",
   "reception.pune@zycus.com",
   "reception.blr@zycus.com",
+  "Reception@zycusitis.onmicrosoft.com"
 ].map((email) => email.toLowerCase());
 
 export const isAssetRestrictedUser = (user: User | null | undefined): boolean => {
