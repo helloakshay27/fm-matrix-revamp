32fc11e1cb95fe39580339bdf5238e2b0f62877e
vaishnavim70 <vaishnavimugale70@gmail.com>
2026-05-15 18:12:54 +0530

    app and heder msafe

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -507,7 +507,7 @@ export const Header = () => {
                 </button>
               )}
 
-              {/* {isViSite && selectedCompany?.id !== 294 && !isWebSite && (
+              {/* {isViSite && selectedCompany?.id !== 294 || selectedCompany?.id === 145) && !isWebSite && (
                 <button
                   onClick={() => navigate("/msafedashboard")}
                   className="flex items-center gap-2 px-3 py-1.5 text-sm font-medium text-[#1a1a1a] hover:text-[#C72030] hover:bg-[#f6f4ee] rounded-lg transition-colors"
@@ -517,7 +517,7 @@ export const Header = () => {
                 </button>
               )}
 
-              {!isViSite && selectedCompany?.id !== 294 && !isWebSite && (
+              {!isViSite && selectedCompany?.id !== 294 || selectedCompany?.id === 145) && !isWebSite && (
                 <button
                   onClick={() =>
                     window.open(
