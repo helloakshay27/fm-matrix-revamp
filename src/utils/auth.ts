443f3d2da67f8c1eb98a352624f4735578fd73ab
omkar chavan <omkarchavan3798@gmail.com>
2026-06-19 15:04:05 +0530

    auth changes

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -267,7 +267,7 @@ export const getOrganizationsByEmail = async (
 ): Promise<Organization[]> => {
   if (isOmanSite || isFmSite) {
     const response = await fetch(
-      `https://live-api.gophygital.work/api/users/get_organizations_by_email.json?email=${email}`
+      `https://uat.lockated.com/api/users/get_organizations_by_email.json?email=${email}`
     );
     if (!response.ok) {
       throw new Error("Failed to fetch organizations");
