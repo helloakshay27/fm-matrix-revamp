d2eb099643919b538bdf79cf240b614201394aa9
Gunjan Mahara <gunjanmahara2@gmail.com>
2026-07-27 20:13:04 +0530

    Fix toggle

--- src/pages/settings/DepartmentDashboard.tsx
+++ src/pages/settings/DepartmentDashboard.tsx
@@ -281,7 +281,7 @@ export const DepartmentDashboard = () => {
                       <Switch
                         checked={department.status}
                         onCheckedChange={() => toggleStatus(department.id)}
-                        className="data-[state=checked]:bg-green-500 data-[state=unchecked]:bg-gray-300"
+                        className="data-[state=unchecked]:bg-gray-300"
                       />
                     </div>
                   </div>
