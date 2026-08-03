12ba9e68c9f21e60e9fd6364fb7e8c3528e15eb8
Atharv Karnekar <karnekaratharv12@gmail.com>
2026-08-03 15:35:57 +0530

    fix: add additional org_id check for layout access

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -242,6 +242,7 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
       org_id === "67" ||
       org_id === "112" ||
       org_id === "87" ||
+      org_id === "82" ||
       userEmail === "sumanta.karmakar@ltimindtree.com" ||
       userEmail === "ubaid.hashmat@lockated.com" ||
       userEmail === "besis69240@azeriom.com" ||
@@ -358,8 +359,9 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
       org_id === "3" ||
       org_id === "67" ||
       org_id === "112" ||
-     org_id === "87" ||
-     userEmail === "sumanta.karmakar@ltimindtree.com" ||
+      org_id === "87" ||
+      org_id === "82" ||
+      userEmail === "sumanta.karmakar@ltimindtree.com" ||
       userEmail === "ubaid.hashmat@lockated.com" ||
       userEmail === "besis69240@azeriom.com" ||
       userEmail === "megipow156@aixind.com" ||
